6e979b6f20d332fc25ddad3a40670aab0e59aceb
Tadeas Petak <tadeaspetak@gmail.com>
2016-08-24 14:55:22 +0200

    Make sure production version of React is used where appropriate.

--- webpack.config.js
+++ webpack.config.js
@@ -61,6 +61,7 @@ var config = {
     //set the BROWSER variable to true (useful for determining context in the shared code base)
     new webpack.DefinePlugin({
       "process.env": {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV ? process.env.NODE_ENV : 'development'),
         BROWSER: JSON.stringify(true)
       }
     })
